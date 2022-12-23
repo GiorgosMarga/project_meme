0e86717d6933a7e5634318d3c666bd22dbff5a96
Γιωργος Μαργα <giorgosmarga509@gmail.com>
2022-12-23 17:06:00 +0200

    removed cluster

--- backend/index.js
+++ backend/index.js
@@ -29,57 +29,56 @@ const errorHandlerMiddleware = require("./middlewares/error-handler");
 
 const cookieParser = require("cookie-parser");
 const cookieMiddleware = require("./middlewares/cookie-handler.js");
-if (cluster.isMaster) {
-  console.log(`Number of CPUs is ${totalCPUs}`);
-  console.log(`Master ${process.pid} is running`);
+// if (cluster.isMaster) {
+//   console.log(`Number of CPUs is ${totalCPUs}`);
+//   console.log(`Master ${process.pid} is running`);
 
-  // Fork workers.
-  for (let i = 0; i < totalCPUs; i++) {
-    cluster.fork();
+//   // Fork workers.
+//   for (let i = 0; i < totalCPUs; i++) {
+//     cluster.fork();
+//   }
+
+//   cluster.on("exit", (worker, code, signal) => {
+//     console.log(`worker ${worker.process.pid} died`);
+//     console.log("Let's fork another worker!");
+//     cluster.fork();
+//   });
+// }
+app.use(cookieParser(process.env.COOKIE_SIGN, { sameSite: "none" }));
+app.use(express.json());
+app.use(fileUpload({ useTempFiles: true }));
+app.use(
+  cors({
+    origin: [
+      "http://192.168.1.4:3000",
+      "http://192.168.1.4:3001",
+      "http://localhost:3001",
+      "http://localhost:3000",
+      "https://project-meme-frontend.vercel.app",
+    ],
+    credentials: true,
+  })
+);
+app.use(cookieMiddleware);
+app.get("/api/v1/config", (req, res) => res.json({ totalCPUs }));
+app.get("/api/v1", (req, res) => {
+  res.send("Meme Project API");
+});
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/answers", answerRouter);
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+const start = async (uri) => {
+  try {
+    await connectDB(uri);
+    app.listen(PORT, () =>
+      console.log(`Server ${process.pid} is listening on port ${PORT}...`)
+    );
+  } catch (err) {
+    console.error(err);
   }
+};
 
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-    console.log("Let's fork another worker!");
-    cluster.fork();
-  });
-} else {
-  app.use(cookieParser(process.env.COOKIE_SIGN, { sameSite: "none" }));
-  app.use(express.json());
-  app.use(fileUpload({ useTempFiles: true }));
-  app.use(
-    cors({
-      origin: [
-        "http://192.168.1.4:3000",
-        "http://192.168.1.4:3001",
-        "http://localhost:3001",
-        "http://localhost:3000",
-        "https://project-meme-frontend.vercel.app",
-      ],
-      credentials: true,
-    })
-  );
-  app.use(cookieMiddleware);
-  app.get("/api/v1/config", (req, res) => res.json({ totalCPUs }));
-  app.get("/api/v1", (req, res) => {
-    res.send("Meme Project API");
-  });
-  app.use("/api/v1/users", userRouter);
-  app.use("/api/v1/posts", postRouter);
-  app.use("/api/v1/comments", commentRouter);
-  app.use("/api/v1/answers", answerRouter);
-  app.use(notFoundMiddleware);
-  app.use(errorHandlerMiddleware);
-  const start = async (uri) => {
-    try {
-      await connectDB(uri);
-      app.listen(PORT, () =>
-        console.log(`Server ${process.pid} is listening on port ${PORT}...`)
-      );
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  start(process.env.MONGO_URI);
-}
+start(process.env.MONGO_URI);
